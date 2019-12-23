c99683d85d36af5338012481fc3342144f050e23
Oleg Lucash <oleg.lucas@amdaris.com>
2019-12-23 17:03:08 +0200

    Added connection string to appsettings. Added DbContext

---
+++ src/OnlineBookShop.API/OnlineBookShopDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineBookShop.Domain;
+using OnlineBookShop.Domain.EFMapping;
+
+namespace OnlineBookShop.API
+{
+    public class OnlineBookShopDbContext: DbContext
+    {
+        public OnlineBookShopDbContext(DbContextOptions<OnlineBookShopDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+
+        public DbSet<Review> Reviews { get; set; }
+
+        public DbSet<PriceOffer> PriceOffers { get; set; }
+
+        public DbSet<Author> Authors { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            var assembly = typeof(BookConfig).Assembly;
+            modelBuilder.ApplyConfigurationsFromAssembly(assembly);
+        }
+
+    }
+}
--- src/OnlineBookShop.API/Startup.cs
+++ src/OnlineBookShop.API/Startup.cs
@@ -1,14 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace OnlineBookShop.API
 {
@@ -24,6 +19,10 @@ namespace OnlineBookShop.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<OnlineBookShopDbContext>(optionBuilder => 
+            {
+                optionBuilder.UseSqlServer(Configuration.GetConnectionString("OnlineBookShopConnection"));
+            });
             services.AddControllers();
         }
 
