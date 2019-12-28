b4b66a2a467540bfe49169fa7f3c9b7c56c364d0
Oleg Lucas <oleg.lucash@gmail.com>
2019-12-29 00:04:57 +0200

    Added two pages. Configured routing

--- src/OnlineBookShop.Angular/src/app/app.module.ts
+++ src/OnlineBookShop.Angular/src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BookListComponent } from './book-list/book-list.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'books', component: BookListComponent }
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      BookListComponent,
+      HomeComponent,
+      BookListComponent
+   ],
+   imports: [
+      BrowserModule,
+      RouterModule.forRoot(
+        appRoutes
+      )
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
