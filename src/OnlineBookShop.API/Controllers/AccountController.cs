57f79c7b13aa860b9bf3b5509868baff736058dc
Oleg Lucash <oleg.lucas@amdaris.com>
2020-02-20 18:52:48 +0200

    Added UserManager and SignInManager for authentication

--- src/OnlineBookShop.API/Controllers/AccountController.cs
+++ src/OnlineBookShop.API/Controllers/AccountController.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using OnlineBookShop.API.Configuration;
 using OnlineBookShop.API.Dtos.Account;
+using OnlineBookShop.Domain.Auth;
 
 namespace OnlineBookShop.API.Controllers
 {
@@ -16,16 +19,23 @@ namespace OnlineBookShop.API.Controllers
     public class AccountController : ControllerBase
     {
         private readonly AuthOptions _authenticationOptions;
-        public AccountController(IOptions<AuthOptions> authenticationOptions)
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public AccountController(IOptions<AuthOptions> authenticationOptions, UserManager<User> userManager, SignInManager<User> signInManager)
         {
             _authenticationOptions = authenticationOptions.Value;
+            _signInManager = signInManager;
+            _userManager = userManager;
         }
 
         [AllowAnonymous]
         [HttpPost("login")]
-        public IActionResult Login(UserForLoginDto userForLoginDto)
+        public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
-            if (userForLoginDto.Username == "admin" && userForLoginDto.Password == "Qwerty")
+            var checkingPasswordResult = await _signInManager.PasswordSignInAsync(userForLoginDto.Username, userForLoginDto.Password, false, false);
+
+            if (checkingPasswordResult.Succeeded)
             {
                 var signinCredentials = new SigningCredentials(_authenticationOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256);
                 var jwtSecurityToken = new JwtSecurityToken(
